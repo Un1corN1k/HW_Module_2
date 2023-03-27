c0384e2caaf3a9a9845abddbe6fbeb991c25e61e
Un1corN1k <unicorn.in.pants@gmail.com>
2023-03-27 21:22:10 +0300

    done task

--- home_work.sql
+++ home_work.sql
@@ -143,6 +143,41 @@ ALTER SEQUENCE public.fav_movie_id_seq OWNED BY public.fav_movie.id;
 
 
 --
+-- Name: for_truncate; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.for_truncate (
+    id integer NOT NULL,
+    text character varying(200),
+    number integer
+);
+
+
+ALTER TABLE public.for_truncate OWNER TO postgres;
+
+--
+-- Name: for_truncate_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.for_truncate_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.for_truncate_id_seq OWNER TO postgres;
+
+--
+-- Name: for_truncate_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.for_truncate_id_seq OWNED BY public.for_truncate.id;
+
+
+--
 -- Name: inner_join; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -185,6 +220,13 @@ ALTER TABLE ONLY public.fav_director ALTER COLUMN id SET DEFAULT nextval('public
 --
 
 ALTER TABLE ONLY public.fav_movie ALTER COLUMN id SET DEFAULT nextval('public.fav_movie_id_seq'::regclass);
+
+
+--
+-- Name: for_truncate id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.for_truncate ALTER COLUMN id SET DEFAULT nextval('public.for_truncate_id_seq'::regclass);
 
 
 --
@@ -242,6 +284,14 @@ COPY public.fav_movie (id, movie, release, star) FROM stdin;
 
 
 --
+-- Data for Name: for_truncate; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.for_truncate (id, text, number) FROM stdin;
+\.
+
+
+--
 -- Data for Name: inner_join; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -279,6 +329,13 @@ SELECT pg_catalog.setval('public.fav_director_id_seq', 3, true);
 --
 
 SELECT pg_catalog.setval('public.fav_movie_id_seq', 3, true);
+
+
+--
+-- Name: for_truncate_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.for_truncate_id_seq', 1, true);
 
 
 --
